ede40674e5e9b769ab0304debb2fe3e5a1717a3f
ronnyjobb <ronliejobb@hotmail.com>
2020-06-05 05:27:01 -0700

    Added dev file

---
+++ TaskGitHun.ps1
@@ -0,0 +1,11 @@
+﻿iex "git checkout -b dev2"
+
+for ($i = 1; $i -lt 8; $i++)
+{
+New-Item "dev_2_$i.txt"
+iex  "git add ."
+iex  "git commit -m 'Added dev file'"
+}
+
+iex "git checkout master"
+iex "git merge dev2 --no-ff"
